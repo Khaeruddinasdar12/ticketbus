d50090a1c2297f78447c3c1b715d11b265eb9421
Khaeruddin Asdar <khaeruddinasdar12@gmail.com>
2020-01-25 21:04:36 +0800

    pembuatan model dan deklarasi tabelnya

---
+++ app/PivotBusRute.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PivotBusRute extends Model
+{
+    protected $table = 'pivot_bus_rutes';
+}
---
+++ app/TipeBus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TipeBus extends Model
+{
+    protected $table = 'tipebus';
+}

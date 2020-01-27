8894d7335d613861ec7ddf74b7859a4cc16f8823
adheee <adheejr77@gmail.com>
2020-01-28 02:15:18 +0800

    add page data-bus, modified template and route in managemen-bus

--- resources/views/admin/managemenbus.blade.php
+++ resources/views/admin/managemenbus.blade.php
@@ -68,7 +68,7 @@ Manajemen Bus
                 <input type="text" class="form-control" id="harga">
               </div>
 
-              <div class="form-group">
+              <div class="form-group" style="margin-top: 45px;">
                 <button type="reset" class="btn btn-secondary float-left"><i class="nav-icon fas fa-sync-alt"></i> Reset</button>
                 <button type="submit" class="btn btn-primary float-right"><i class="nav-icon fas fa-plus"></i> Tambah</button>
               </div>
@@ -83,34 +83,138 @@ Manajemen Bus
       <!-- right column -->
       <div class="col-md-6">
         <!-- general form elements -->
-        <div class="card card-success">
-          <div class="card-header">
-            <h3 class="card-title">Tambah Tipe Bus</h3>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card card-info">
+              <div class="card-header">
+                <h3 class="card-title">Tambah Tipe Bus</h3>
 
-            <div class="card-tools">
-              <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-                <i class="fas fa-minus"></i></button>
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                    <i class="fas fa-minus"></i></button>
+                </div>
+
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+
+              <div class="card-body">
+                <form role="form">
+                  <div class="form-group">
+                    <label for="tipebus">Nama Tipe</label>
+                    <input type="text" class="form-control" id="tipebus">
+                  </div>
+
+                  <div class="form-group">
+                    <button type="reset" class="btn btn-secondary float-left"><i class="nav-icon fas fa-sync-alt"></i> Reset</button>
+                    <button type="submit" class="btn btn-info float-right"><i class="nav-icon fas fa-plus"></i> Tambah</button>
+                  </div>
+
+                </form>
+              </div>
+              <!-- /.card-body -->
             </div>
-
           </div>
-          <!-- /.card-header -->
-          <!-- form start -->
+        </div>
 
-          <div class="card-body">
-            <form role="form">
-              <div class="form-group">
-                <label for="tipebus">Nama Tipe</label>
-                <input type="text" class="form-control" id="tipebus">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card card-info">
+              <div class="card-header">
+                <h3 class="card-title">Tambah Route</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                    <i class="fas fa-minus"></i></button>
+                </div>
+
               </div>
+              <!-- /.card-header -->
+              <!-- form start -->
 
-              <div class="form-group">
-                <button type="reset" class="btn btn-secondary float-left"><i class="nav-icon fas fa-sync-alt"></i> Reset</button>
-                <button type="submit" class="btn btn-success float-right"><i class="nav-icon fas fa-plus"></i> Tambah</button>
+              <div class="card-body">
+                <form role="form">
+                  <div class="form-group">
+                    <label for="namaroute">Nama Route</label>
+                    <input type="text" class="form-control" id="namaroute">
+                  </div>
+
+                  <div class="form-group">
+                    <button type="reset" class="btn btn-secondary float-left"><i class="nav-icon fas fa-sync-alt"></i> Reset</button>
+                    <button type="submit" class="btn btn-info float-right"><i class="nav-icon fas fa-plus"></i> Tambah</button>
+                  </div>
+
+                </form>
               </div>
-
-            </form>
+              <!-- /.card-body -->
+            </div>
           </div>
-          <!-- /.card-body -->
+        </div>
+
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card card-info">
+              <div class="card-header">
+                <h3 class="card-title">Tambah Pivot</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                    <i class="fas fa-minus"></i></button>
+                </div>
+
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+
+              <div class="card-body">
+                <form role="form">
+                  <div class="row">
+                    <div class="col-sm-4">
+                      <!-- text input -->
+                      <div class="form-group">
+                        <label for="inputStatus">Nama Bus</label>
+                        <select class="form-control custom-select">
+                          <option selected disabled>Pilih tipe</option>
+                          <option>Sleeper</option>
+                          <option>Seatbelt</option>
+                          <option>Comfortable</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div class="col-sm-4">
+                      <div class="form-group">
+                        <label for="inputStatus">Tipe Bus</label>
+                        <select class="form-control custom-select">
+                          <option selected disabled>Pilih tipe</option>
+                          <option>Bintang Prima A10</option>
+                          <option>Bintang Prima M70</option>
+                          <option>Bintang Prima C30</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div class="col-sm-4">
+                      <div class="form-group">
+                        <label for="inputStatus">Route</label>
+                        <select class="form-control custom-select">
+                          <option selected disabled>Pilih tipe</option>
+                          <option>Makassar - Bulukumba</option>
+                          <option>Bulukumba - Selayar</option>
+                          <option>Selayar - Maros</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <button type="reset" class="btn btn-secondary float-left"><i class="nav-icon fas fa-sync-alt"></i> Reset</button>
+                    <button type="submit" class="btn btn-info float-right"><i class="nav-icon fas fa-plus"></i> Tambah</button>
+                  </div>
+
+                </form>
+              </div>
+              <!-- /.card-body -->
+            </div>
+          </div>
         </div>
         <!-- /.card -->
       </div>
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Auth::routes();
 // Route::get('/home', 'HomeController@index')->name('home');
 Route::get('dashboard', 'Dashboard@index')->name('index');
 Route::get('managemen-bus', 'ManagemenBus@index')->name('index.bus');
+Route::get('data-bus', 'ManagemenBus@data')->name('data.bus');
 Route::get('managemen-jadwal', 'Jadwal@index')->name('index.jadwal');
 Route::get('data-transaksi', 'Transaksi@index')->name('index.transaksi');
 Route::get('riwayat-transaksi', 'Transaksi@riwayat')->name('riwayat.transaksi');

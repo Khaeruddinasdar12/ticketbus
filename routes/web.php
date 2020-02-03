6eddba2d381765836a67447254e913fb9128ec40
Khaeruddin Asdar <khaeruddinasdar12@gmail.com>
2020-02-04 01:01:00 +0800

    perbaiki editBus dan tambah method delete di controller

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,11 @@ Route::prefix('managemen-bus')->group(function () {
 	Route::put('edit-bus/{id}', 'ManagemenBus@editBus');
 	Route::put('edit-tipe-bus/{id}', 'ManagemenBus@editTipe');
 	Route::put('edit-rute/{id}', 'ManagemenBus@editRute');
-	Route::put('edit-bus/{id}', 'ManagemenBus@editPivot');
+
+	Route::delete('delete-bus/{id}', 'ManagemenBus@editBus');
+	Route::delete('delete-tipe-bus/{id}', 'ManagemenBus@editTipe');
+	Route::delete('delete-rute/{id}', 'ManagemenBus@editRute');
+	Route::delete('delete-bus/{id}', 'ManagemenBus@editPivot');
 });
 
 // RUTE MANAGEMEN JADWAL

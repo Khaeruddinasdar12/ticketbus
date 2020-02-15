c92ad8d469001414b93fe3daf091c66120a197b3
adheee <adheejr77@gmail.com>
2020-02-15 16:23:58 +0800

    modified dashboard

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -75,8 +75,8 @@
       <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div class="image">
-            <img src="{{ asset('admin/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+          <div class="image image-s">
+            <i class="fa fa-user-circle"></i>
           </div>
           <div class="info">
             <a href="#" class="d-block">{{ Auth::user()->name }} </a>
@@ -377,5 +377,12 @@
     }
   </script>
 </body>
+
+<style>
+  .image-s i {
+    font-size: 30px !important;
+    color: #ffff;
+  }
+</style>
 
 </html>

5736f0012ac48da45a6bd18f73b4b7cd372d164c
adheee <adheejr77@gmail.com>
2020-02-18 00:44:52 +0800

    tambah jumlah data bus di dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,7 +6,7 @@ Dashboard
 @section('content')
 <!-- Content Header (Page header) -->
 <style>
-  .col-md-4 .small-box {
+  .col-md-6 .small-box {
     height: 170px !important;
     cursor: pointer;
     transition: 1s;
@@ -16,11 +16,11 @@ Dashboard
     font-size: 100px !important;
   }
 
-  .col-md-4 .small-box:hover .icon i {
+  .col-md-6 .small-box:hover .icon i {
     font-size: 120px !important;
   }
 
-  .col-md-4 .small-box:hover {
+  .col-md-6 .small-box:hover {
     position: relative;
     bottom: 10px;
     box-shadow: 0 15px 15px #adacad;
@@ -49,6 +49,24 @@ Dashboard
   .small-box .inner p {
     color: #fff;
   }
+
+  .bg-custom-1,
+  .bg-custom-2,
+  .bg-custom-3 {
+    color: #fff;
+  }
+
+  .bg-custom-1 {
+    background-color: #4a47ff;
+  }
+
+  .bg-custom-2 {
+    background-color: #ff6947;
+  }
+
+  .bg-custom-3 {
+    background-color: #ff47ac;
+  }
 </style>
 
 <div class="content-header">
@@ -69,55 +87,77 @@ Dashboard
 <section class="content mt-4">
   <div class="container-fluid">
     <div class="row card-box">
-      <div class="col-md-4">
-        <!-- small box -->
-        <a href="#">
-          <div class="small-box bg-custom1">
-            <div class="inner">
-              <div class="container">
-                <h3>150</h3>
-                <p>User yang bertransaksi</p>
+      <div class="col-md-8">
+        <div class="row">
+          <div class="col-md-6">
+            <!-- small box -->
+            <a href="#">
+              <div class="small-box bg-custom1">
+                <div class="inner">
+                  <div class="container">
+                    <h3>150</h3>
+                    <p>User yang bertransaksi</p>
+                  </div>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-person-add"></i>
+                </div>
               </div>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div>
+            </a>
           </div>
-        </a>
+          <div class="col-md-6">
+            <!-- small box -->
+            <a href="#">
+              <div class="small-box bg-custom3">
+                <div class="inner">
+                  <div class="container">
+                    <h3>150</h3>
+                    <p>Jadwal bus</p>
+                  </div>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-person-add"></i>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
       </div>
-      <!-- ./col -->
       <div class="col-md-4">
-        <!-- small box -->
-        <a href="#">
-          <div class="small-box bg-custom2">
-            <div class="inner">
-              <div class="container">
-                <h3>150</h3>
-                <p>User yang bertransaksi</p>
-              </div>
-            </div>
-            <div class="icon">
-              <i class="ion ion-stats-bars"></i>
-            </div>
+        <!-- Info Boxes Style 2 -->
+        <div class="info-box">
+          <span class="info-box-icon bg-custom-1 elevation-1"><i class="fas fa-cog"></i></span>
+          <div class="info-box-content">
+            <span class="info-box-text">Jumlah Bus Scania</span>
+            <span class="info-box-number">
+              10
+              <small>%</small>
+            </span>
           </div>
-        </a>
-      </div>
-      <!-- ./col -->
-      <div class="col-md-4">
-        <!-- small box -->
-        <a href="#">
-          <div class="small-box bg-custom3">
-            <div class="inner">
-              <div class="container">
-                <h3>150</h3>
-                <p>Jadwal bus</p>
-              </div>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div>
+        </div>
+
+        <div class="info-box">
+          <span class="info-box-icon bg-custom-2 elevation-1"><i class="fas fa-cog"></i></span>
+          <div class="info-box-content">
+            <span class="info-box-text">Jumlah Bus Sleeper</span>
+            <span class="info-box-number">
+              10
+              <small>%</small>
+            </span>
           </div>
-        </a>
+        </div>
+
+        <div class="info-box">
+          <span class="info-box-icon bg-custom-3 elevation-1"><i class="fas fa-cog"></i></span>
+          <div class="info-box-content">
+            <span class="info-box-text">Jumlah Bus Suspensi</span>
+            <span class="info-box-number">
+              10
+              <small>%</small>
+            </span>
+          </div>
+        </div>
+
       </div>
     </div>
     <!-- /.row -->

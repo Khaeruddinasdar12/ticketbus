282a2de77945a90ecde1f9f2b087a003f28bfd26
adheee <adheejr77@gmail.com>
2020-02-19 21:36:12 +0800

    edit invoice page

--- app/Http/Controllers/Invoice.php
+++ app/Http/Controllers/Invoice.php
@@ -8,29 +8,31 @@ use DB;
 class Invoice extends Controller
 {
 	public function __construct()
-    {
-        $this->middleware('auth');
-    }
+	{
+		$this->middleware('auth');
+	}
 
-    public function invoice($id) {
+	public function invoice($id)
+	{
 		\App\Transaksi::findOrFail($id);
 		$data = DB::table('transaksis')
-	            ->join('users', 'transaksis.id_customer', '=', 'users.id')
-	            ->join('jadwals', 'transaksis.id_jadwal', '=', 'jadwals.id')
-	            ->join('pivot_bus_rutes', 'jadwals.id_bus_rute', '=', 'pivot_bus_rutes.id')
-	            ->join('rutes', 'pivot_bus_rutes.id_rute', '=', 'rutes.id')
-	            ->join('bus', 'pivot_bus_rutes.id_bus', '=', 'bus.id')
-	            ->join('tipebus', 'bus.id_tipebus', '=', 'tipebus.id')
-	            ->select('transaksis.id', 'transaksis.order_code', 'transaksis.barcode', 'users.name', 'jadwals.tanggal', 'jadwals.jam', 'bus.nama as namabus', 'bus.deskripsi', 'rutes.rute', 'tipebus.nama as tipebus', 'pivot_bus_rutes.harga', 'transaksis.no_kursi', 'transaksis.status_bayar')
-	            ->where('transaksis.status_bayar', 'sudah')
-	            ->where('transaksis.id', $id)
-	            ->get();
+			->join('users', 'transaksis.id_customer', '=', 'users.id')
+			->join('jadwals', 'transaksis.id_jadwal', '=', 'jadwals.id')
+			->join('pivot_bus_rutes', 'jadwals.id_bus_rute', '=', 'pivot_bus_rutes.id')
+			->join('rutes', 'pivot_bus_rutes.id_rute', '=', 'rutes.id')
+			->join('bus', 'pivot_bus_rutes.id_bus', '=', 'bus.id')
+			->join('tipebus', 'bus.id_tipebus', '=', 'tipebus.id')
+			->select('transaksis.id', 'transaksis.order_code', 'transaksis.barcode', 'users.name', 'jadwals.tanggal', 'jadwals.jam', 'bus.nama as namabus', 'bus.deskripsi', 'rutes.rute', 'tipebus.nama as tipebus', 'pivot_bus_rutes.harga', 'transaksis.no_kursi', 'transaksis.status_bayar')
+			->where('transaksis.status_bayar', 'sudah')
+			->where('transaksis.id', $id)
+			->get();
 		// return $data;
 		return view('admin.invoice', ['data' => $data]);
 	}
 
-	public function print($id) {
-		
+	public function print($id)
+	{
+
 		return view('admin.invoice');
 	}
 }
--- resources/views/admin/invoice.blade.php
+++ resources/views/admin/invoice.blade.php
@@ -41,63 +41,54 @@ Transaksi
           </div>
           <hr>
 
-          <div class="row invoice-info">
-            <div class="col-12">
+          <div class="row mt-5">
+            <div class="col-md-2 offset-md-1">
               @foreach($data as $datas)
-              <img src="{{asset('storage/'.$data->barcode)}}" alt="">
-              @endforeach
+              <img src="{{asset('storage/'.$datas->barcode)}}" alt="Astec Code" style="width: 200px;">
             </div>
-            <!-- /.col -->
-          </div>
-          <!-- /.row -->
 
-          <!-- Table row -->
-          <div class="row">
-            <div class="col-12 table-responsive">
-              <table class="table table-striped">
-                <thead>
-                  <tr>
-                    <th>Qty</th>
-                    <th>Product</th>
-                    <th>Serial #</th>
-                    <th>Description</th>
-                    <th>Subtotal</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1</td>
-                    <td>Call of Duty</td>
-                    <td>455-981-221</td>
-                    <td>El snort testosterone trophy driving gloves handsome</td>
-                    <td>$64.50</td>
-                  </tr>
-                  <tr>
-                    <td>1</td>
-                    <td>Need for Speed IV</td>
-                    <td>247-925-726</td>
-                    <td>Wes Anderson umami biodiesel</td>
-                    <td>$50.00</td>
-                  </tr>
-                  <tr>
-                    <td>1</td>
-                    <td>Monsters DVD</td>
-                    <td>735-845-642</td>
-                    <td>Terry Richardson helvetica tousled street art master</td>
-                    <td>$10.70</td>
-                  </tr>
-                  <tr>
-                    <td>1</td>
-                    <td>Grown Ups Blue Ray</td>
-                    <td>422-568-642</td>
-                    <td>Tousled lomo letterpress</td>
-                    <td>$25.99</td>
-                  </tr>
-                </tbody>
+            <div class="col-md-8 table-responsive ml-4">
+              <table class="table">
+                <tr>
+                  <th>Nama</th>
+                  <td>:</td>
+                  <td>{{ $datas->name }}</td>
+                </tr>
+
+                <tr>
+                  <th>Email</th>
+                  <td>:</td>
+                </tr>
+
+                <tr>
+                  <th>Tanggal Berangkat</th>
+                  <td>:</td>
+                </tr>
+
+                <tr>
+                  <th>Nomor Kursi</th>
+                  <td>:</td>
+                </tr>
+
+                <tr>
+                  <th>Tipe Bus</th>
+                  <td>:</td>
+                </tr>
+
+                <tr>
+                  <th>Nomor Polisi / DD</th>
+                  <td>:</td>
+                </tr>
+
+                <tr>
+                  <th>Jam Berangkat</th>
+                  <td>:</td>
+                </tr>
+                @endforeach
               </table>
             </div>
-
           </div>
+          <!-- /.row -->
         </div>
       </div>
     </div>

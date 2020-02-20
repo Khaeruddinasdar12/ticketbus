a35f8cd5d2f52057cb2fe324c798085e2e967fea
Khaeruddin Asdar <khaeruddinasdar12@gmail.com>
2020-02-21 01:10:10 +0800

    api jadwals search

--- app/Http/Controllers/api/Jadwal.php
+++ app/Http/Controllers/api/Jadwal.php
@@ -34,6 +34,7 @@ class Jadwal extends Controller
     {
         $rute     = $request->id_rute;
         $tanggal  = $request->tanggal;
+        // return $request->tanggal;
 
         if ($rute != '' && $tanggal != '') {
           $data = DB::table('jadwals')
@@ -56,7 +57,7 @@ class Jadwal extends Controller
           ->join('tipebus', 'bus.id_tipebus', '=', 'tipebus.id')
           ->rightJoin('kursis', 'jadwals.id', 'kursis.id_jadwal')
           ->select('jadwals.id', 'jadwals.tanggal', 'jadwals.jam', 'bus.nama as namabus', 'bus.deskripsi', 'rutes.id as rute_id', 'rutes.rute', 'tipebus.nama as tipebus', 'jadwals.status', 'pivot_bus_rutes.harga', DB::raw('count(case when kursis.status = "kosong" then 1 end)as kursi_kosong'))
-          ->where('rutes.id', $$request->id_rute) // id rute
+          ->where('rutes.id', $rute) // id rute
           ->where('jadwals.status', 'belum')
           ->groupBy('jadwals.id', 'jadwals.tanggal', 'jadwals.jam', 'namabus', 'bus.deskripsi', 'rutes.rute', 'tipebus', 'jadwals.status', 'pivot_bus_rutes.harga' , 'rute_id')
           ->get();
@@ -68,7 +69,7 @@ class Jadwal extends Controller
           ->join('tipebus', 'bus.id_tipebus', '=', 'tipebus.id')
           ->rightJoin('kursis', 'jadwals.id', 'kursis.id_jadwal')
           ->select('jadwals.id', 'jadwals.tanggal', 'jadwals.jam', 'bus.nama as namabus', 'bus.deskripsi', 'rutes.id as rute_id', 'rutes.rute', 'tipebus.nama as tipebus', 'jadwals.status', 'pivot_bus_rutes.harga', DB::raw('count(case when kursis.status = "kosong" then 1 end)as kursi_kosong'))
-          ->where('jadwals.tanggal', $request->tanggal) 
+          ->where('jadwals.tanggal', $tanggal) 
           ->where('jadwals.status', 'belum')
           ->groupBy('jadwals.id', 'jadwals.tanggal', 'jadwals.jam', 'namabus', 'bus.deskripsi', 'rutes.rute', 'tipebus', 'jadwals.status', 'pivot_bus_rutes.harga', 'rute_id')
           ->get();
